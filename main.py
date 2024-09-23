79b7cc4417dbf76c2e56a1e4d90179751e1db70b
Serg18353 <mikholenok@yandex.ru>
2024-09-23 22:39:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+var = ('@','.com','.ru','.net')
+recipient = ('vasyok1337@gmail.com')
+sender = ('niversity.help@gmail.com')
+
+    if '@' not in recipient:
+        print('Письмо успешно отправлено с адреса university.help@gmail.com на адрес vasyok1337@gmail.com')
+
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ!')
+
+
+
+#send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+#send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+#send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+#send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
